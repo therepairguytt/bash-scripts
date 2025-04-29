59504c9a0e0d3e6c0f80e5ce76a354bec09202a2
TheRepairGuyTT <151693218+therepairguytt@users.noreply.github.com>
2025-04-29 02:13:37 -0400

    Update reverse_proxy.sh

--- reverse_proxy.sh
+++ reverse_proxy.sh
@@ -39,7 +39,8 @@ log "INFO" "The domains [${SERVER_DOMAINS}] will proxy to $PROXY_TARGET"
 sudo tee "$CONFIG_FILE" > /dev/null <<EOF
 server {
     listen 80;
-	listen [::]:80
+    listen [::]:80
+    
     server_name ${SERVER_DOMAINS};
 
     location / {
@@ -48,7 +49,7 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
-		client_max_body_size 0;
+        client_max_body_size 0;
     }
 }
 EOF
