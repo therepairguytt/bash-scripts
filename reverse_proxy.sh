ee2b1f3742dac6d88019eea7aec71151f76c6625
TheRepairGuyTT <151693218+therepairguytt@users.noreply.github.com>
2025-05-08 23:10:44 -0400

    Update reverse_proxy.sh

--- reverse_proxy.sh
+++ reverse_proxy.sh
@@ -52,7 +52,7 @@ server {
         proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
-		proxy_http_version 1.1;
+        proxy_http_version 1.1;
         client_max_body_size 0;
 		
     }
